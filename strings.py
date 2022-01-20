f88cd7363dc72c9cba85ae71febc0c43ff3717ba
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-01-20 14:48:17 -0500

    slicing

--- strings.py
+++ strings.py
@@ -26,3 +26,12 @@ string_2 = "place"
 # %s as a replacement and a % after string to signift end. Like a psql query
 
 print ("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
+
+my_string = "This is MY string!"
+print(my_string[0:4]) # From the start till before the 4th index
+print(my_string[1:7])
+print(my_string[8:len(my_string)]) # From the 8th index till the end
+
+print(my_string[0:7:2])  # A step of 2 meaning every second letter in that slice
+print(my_string[0:7:5])  # A step of 5
+print(my_string[::-1])  # The whole string in reverse (step is -1)
