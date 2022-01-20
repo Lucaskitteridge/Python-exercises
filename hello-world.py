3cea665a2270bb954c92908a9dc2c4627af11ec2
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-01-20 14:09:51 -0500

    basic string syntax

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,28 @@
 print("This line will be printed.")
+
+print("Hello" + "Lucas")
+
+todays_date = "January 20th"
+
+print(todays_date)
+
+print(7/2)
+string = """This is
+a multilevel
+string using triple quotes"""
+print(string)
+
+number1 = "100"
+number2 = "10"
+number3 = 13
+
+string_addition = number1 + number2
+number_addition = int(number1) + int(number2)
+
+print(string_addition)
+print(number_addition)
+
+#must convert number to string before adding together
+print("I am a " + str(number3) + " year old cheese")
+
+# comments are added with hash
