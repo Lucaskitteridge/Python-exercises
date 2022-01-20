9dde8435e32311ad9c3eb20af7a6a253f63de07d
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-01-20 16:22:24 -0500

    madlibs added

---
+++ mad_libs.py
@@ -0,0 +1,6 @@
+color = input("Pick color ")
+color2 = input("Pick second color ")
+verb = input("Pick verb ")
+madlib = "Roses are " + color + ", violets are " + color2 + ", I " + verb + " you"
+
+print(madlib)
