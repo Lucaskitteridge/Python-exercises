29c6de33ed533f79b485569d53428233b48a930c
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-01-20 14:22:57 -0500

    template literals

--- strings.py
+++ strings.py
@@ -17,3 +17,12 @@ print(len(parrot)) #length
 print(parrot.lower()) #lowercase
 print(parrot.upper()) #uppercase
 print(str(14)) #number to string
+
+# dot notation only works with strings but () words with other types
+
+string_1 = "Camelot"
+string_2 = "place"
+
+# %s as a replacement and a % after string to signift end. Like a psql query
+
+print ("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
