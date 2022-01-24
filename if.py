07f8d8d2ab1639dce6902f1d522d72e7f982b733
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-01-24 18:04:23 -0500

    if statement syntax

---
+++ if.py
@@ -0,0 +1,12 @@
+is_male = True
+is_tall = True
+
+#indent for if statement as well
+
+#or is js equivalent of ||
+#and is js equivalent of &&
+
+if is_male and is_tall:
+  print("You are a male or tall or both")
+else:
+  print("You are not a male or not tall or both")
