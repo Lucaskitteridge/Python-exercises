5947a7b2b143ce8616f01fad3ae878151465735d
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-01-25 15:13:42 -0500

    elif and comparisons in if statements

--- if.py
+++ if.py
@@ -10,3 +10,13 @@ if is_male and is_tall:
   print("You are a male or tall or both")
 else:
   print("You are not a male or not tall or both")
+
+  #elif fo else if
+
+def max_num(num1, num2, num3):
+  if num1 >= num2 and num1 >= num3:
+    return num1
+  elif num2 >= num1 and num2 >= num3:
+    return num2
+  else:
+    return num3
