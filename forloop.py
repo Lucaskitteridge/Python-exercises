a8f015dee9223ee8198b820a34587dfd51f0a8a4
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-02-01 11:26:07 -0500

    basics of for loops

---
+++ forloop.py
@@ -0,0 +1,14 @@
+#similar syntax to JS
+
+for letter in "school":
+  print(letter)
+
+freinds = ["Jess", "Scott", "Mike"]
+
+for freind in freinds:
+  print(freind)
+
+#length of array to get length and then loop through range
+for index in range(len(freinds)):
+  print(freinds[index])
+  print(index)
